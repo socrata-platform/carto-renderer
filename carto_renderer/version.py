39d5b57154ffccec295b3319b7d4e19df86daf91
Kaida Masaki <kaida.masaki@socrata.com>
2015-10-20 10:16:33 -0700

    Bump version to 0.1.0-SNAPSHOT

--- carto_renderer/version.py
+++ carto_renderer/version.py
@@ -2,7 +2,7 @@
 A file just to hold the version number, allows automated version increasing.
 """
 
-SEMANTIC = '0.0.5-SNAPSHOT'
+SEMANTIC = '0.1.0-SNAPSHOT'
 BUILD_TIME = 'UNKNOWN'
 try:
     with open('build-time.txt') as f:
