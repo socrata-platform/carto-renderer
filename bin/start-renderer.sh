3ec24c849393dc0a395c8ed027a3c0eb6c686cfb
Kaida Masaki <kaida.masaki@socrata.com>
2017-09-14 15:59:14 -0700

    Dockerize tests

--- bin/start-renderer.sh
+++ bin/start-renderer.sh
@@ -4,6 +4,18 @@
 cd "$(git rev-parse --show-toplevel 2>/dev/null)" || exit 1
 
 if [ '--dev' = "$1" ]; then
+    MAPNIK_DIR=$(dirname "$(python -c 'import mapnik; print(mapnik.__file__)')")
+     
+    if [ ! -d venv ]; then
+        virtualenv venv
+    fi
+    source venv/bin/activate
+     
+    if [ ! -d venv/lib/python2.7/site-packages/mapnik ]; then
+        ln -s "$MAPNIK_DIR" venv/lib/python2.7/site-packages/
+    fi
+
+    pip install --upgrade --requirement dev-requirements.txt
     PYTHONPATH=. carto_renderer/service.py
 else
     bin/dockerize.sh
--- bin/test.sh
+++ bin/test.sh
@@ -5,22 +5,21 @@ set -ev
 # Change to the project root.
 cd "$(git rev-parse --show-toplevel 2>/dev/null)"
 
-MAPNIK_DIR=$(dirname "$(python -c 'import mapnik; print(mapnik.__file__)')")
+if [ '--dev' = "$1" ]; then
+    MAPNIK_DIR=$(dirname "$(python -c 'import mapnik; print(mapnik.__file__)')")
+     
+    if [ ! -d venv ]; then
+        virtualenv venv
+    fi
+    source venv/bin/activate
+     
+    if [ ! -d venv/lib/python2.7/site-packages/mapnik ]; then
+        ln -s "$MAPNIK_DIR" venv/lib/python2.7/site-packages/
+    fi
 
-VENV_DIR="venv"
-if [ "${HUDSON_HOME}" ]; then
-    VENV_DIR="${HUDSON_HOME}/carto-renderer/${VENV_DIR}"
+     pip install --upgrade --requirement dev-requirements.txt
+     PYTHONPATH=. py.test -v carto_renderer
+else
+    find . -name '*.pyc' -delete    
+    docker build -f .TestDockerfile .
 fi
-
-if [ ! -d "${VENV_DIR}" ]; then
-    virtualenv "${VENV_DIR}"
-fi
-source "${VENV_DIR}"/bin/activate
-
-if [ ! -d "${VENV_DIR}"/lib/python2.7/site-packages/mapnik ]; then
-    ln -s "$MAPNIK_DIR" "${VENV_DIR}"/lib/python2.7/site-packages/
-fi
-
-pip install --upgrade --requirement "dev-requirements.txt"
-
-PYTHONPATH=. py.test -v carto_renderer
