d5217fb1fee85bebd0acb70f8acf07229db43987
Sarah Salinas <sarah.salinas@tylertech.com>
2024-07-15 15:46:59 -0400

    Setting version to 0.1.8-SNAPSHOT

--- carto_renderer/version.py
+++ carto_renderer/version.py
@@ -2,7 +2,7 @@
 A file just to hold the version number, allows automated version increasing.
 """
 
-SEMANTIC = '0.1.7'
+SEMANTIC = '0.1.8-SNAPSHOT'
 BUILD_TIME = 'UNKNOWN'
 try:
     with open('build-time.txt') as f:
